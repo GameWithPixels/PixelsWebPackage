aff9914a3cb1253059a9eb5203ff8993f486ac1f
Olivier Basille <obasille@hotmail.com>
2022-07-18 15:23:30 +0200

    chore: update commitlint & husky versions, check for linting and typescript errors on commit

--- .husky/common.sh
+++ .husky/common.sh
@@ -2,7 +2,7 @@ command_exists () {
   command -v "$1" >/dev/null 2>&1
 }
 
-# Windows 10, Git Bash and Yarn workaround
+# Workaround for Windows 10, Git Bash and Yarn
 if command_exists winpty && test -t 1; then
   exec < /dev/tty
 fi
